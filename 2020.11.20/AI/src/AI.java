b11223af3794d7fc9357a68ff6d0f5b1678eb8e7
LZF <1152870342@qq.com>
2020-11-22 13:29:07 +0800

    优化四个方向的移动

---
+++ 2020.11.20/AI/src/AI.java
@@ -0,0 +1,124 @@
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+import java.io.*;
+public class AI extends MIDlet
+{
+	Display display;
+	MainCanvas mc;
+	public AI(){
+		display=Display.getDisplay(this);
+		mc=new MainCanvas();
+		display.setCurrent(mc);
+	}
+	public void startApp(){
+	}
+	public void destroyApp(boolean unc){
+	}
+	public void pauseApp(){
+	}
+}
+class MainCanvas extends Canvas
+{   Image heroLeftImg[]=new Image[3];
+    Image heroRightImg[]=new Image[3];
+	Image heroUpImg[]=new Image[3];
+	Image heroDownImg[]=new Image[3];
+	Image currentImg;
+	int x,y;
+	int Leftflag,Rightflag,Upflag,Downflag;
+	public MainCanvas(){
+	try
+	{
+		for(int i=0;i<heroLeftImg.length;i++){
+		    heroLeftImg[i]=Image.createImage("/sayo"+i+"2.png");  //左
+		
+		}
+		for(int i=0;i<heroRightImg.length;i++){
+			heroRightImg[i]=Image.createImage("/sayo"+i+"6.png");  //右
+		}
+		for(int i=0;i<heroUpImg.length;i++){
+            heroUpImg[i]=Image.createImage("/sayo"+i+"4.png");  //上
+		}
+		for(int i=0;i<heroDownImg.length;i++){
+		    heroDownImg[i]=Image.createImage("/sayo"+i+"0.png"); //下
+		}
+
+
+		currentImg=heroDownImg[1];
+		x=120;
+		y=120;
+		Leftflag=1;
+		Rightflag=1;
+		Upflag=1;
+		Downflag=1;
+		
+		
+	}
+	catch(IOException e)
+	{
+		e.printStackTrace();
+	 }
+	}
+	public void paint(Graphics g){
+		g.setColor(0,100,100);
+		g.fillRect(0,0,getWidth(),getHeight());
+		g.drawImage(currentImg,x,y,0);
+	}
+	public void keyPressed(int keyCode){
+	int action=getGameAction(keyCode); 
+
+	if(action==LEFT){
+		if(Leftflag==1){
+			currentImg=heroLeftImg[0];
+			Leftflag++;
+		}
+
+		else if(Leftflag==2){
+			currentImg=heroLeftImg[2];
+			Leftflag=1;
+		}
+			System.out.println("向左走");
+				x=x-3;
+	}
+	
+	else if(action==RIGHT){
+		if(Rightflag==1){
+			currentImg=heroRightImg[0];
+			Rightflag++;
+		}
+
+		else if(Rightflag==2){
+			currentImg=heroRightImg[2];
+			Rightflag=1;
+		}
+System.out.println("向右走");
+		x=x+3;
+	}
+	
+	else if(action==UP){
+	if(Upflag==1){
+		currentImg=heroUpImg[0];	
+		Upflag++;
+	}
+		else if(Upflag==2){
+			currentImg=heroUpImg[2];
+			Upflag=1;
+		}
+	System.out.println("向上走");
+			y=y-3;
+	}
+
+	else if (action==DOWN){
+		if(Downflag==1){
+			currentImg=heroDownImg[0];
+			Downflag++;	
+	    }
+		else if(Downflag==2){
+			currentImg=heroDownImg[2];
+			Downflag=1;
+		System.out.println("向下走");
+	     }
+		y=y+3;
+    }   	
+	repaint();
+  }	
+}
